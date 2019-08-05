90b013e7256b105255815641ae21605787d16406
Gerardo Gabriel Rosas Rodríguez <grosas@gdc.ad.com.mx>
2019-08-05 16:48:44 -0500

    2 commit
    Actualizacion

--- clientes.js
+++ clientes.js
@@ -21,20 +21,102 @@ db.clientes.insert([
     { firstName: 'Elvia', lastName: 'Rodriguez'}  
 ])
 
+db.clientes.insert([
+    { firstName: 'Eduardo', lastName: 'Roas'},
+    { firstName: 'Vianey', lastName: 'Roas'},
+    { firstName: 'santos', lastName: 'Rodriguez', age: 20 },  
+    { firstName: 'roberto', lastName: 'Rodriguez', age: 30 },
+    { firstName: 'luis', lastName: 'Rodriguez', age: 10 }  
+])
+
 //----Actualizar Dato
 db.clientes.update(
     { firstName: 'Vianey'},
-    {firstName: 'Vianey', lastName: 'Rosas', gender: 'femela'}
+    { firstName: 'Vianey', lastName: 'Rosas', gender: 'femela'}
 )
 
 //--- Buscar datodos
 db.clientes.find()
 
-//--- Busacar datos con parametro de busqueda
-db.clientes.find({lastName: 'Rosas'})
+//----Mostrar datos formateados
+db.clientes.find().pretty()
 
-//---Insertar un nuevo atributo
+//--- Busacar datos con parametro de busqueda
+db.clientes.find(
+    { lastName: 'Rosas' }
+)
+
+//---Insertar un nuevo atributo con su valor
 db.clientes.update(
-    { _id: ObjectId("5d4796f9b7279d878837b73c")},
+    { _id: ObjectId("5d486d215237c39f52bafdfe")},
     { $set: {age: 60} }
 )
+
+//---Incrementar un valor entero de un atributo.
+db.clientes.update(
+    {_id: ObjectId("5d486d215237c39f52bafdfe")},
+    { $inc: { age: 5 } }
+)
+
+//---Restar un valor de un atributo.
+db.clientes.update(
+    {_id: ObjectId("5d486d215237c39f52bafdfe")},
+    { $inc: { age: -5 } }
+)
+
+//----Eliminar un atributo de una colección
+db.clientes.update(
+    { _id: ObjectId("5d486d215237c39f52bafdfe") },
+    { $unset: { age: 1 } }
+)
+
+//----Eliminar un atributo que coincida con la busqueda
+db.clientes.remove(
+    { firstName : "isabel" }
+)
+
+//----Eliminar un atributo que coincida con la busqueda pero solo uno
+db.clientes.remove(
+    { firstName : "isabel" },
+    { justOne: true }
+)
+ 
+//-----Actualizar valores de los atributos pero si no esta insertar un nuevo dato
+db.clientes.update(
+    { firstName: "isabel" },
+    { firstName: "isabel", lastName: "cristino"},
+    { upsert: true }
+) 
+//-------Renombrar nombre de atributos de la collecion
+db.clientes.update(
+    { primerNombre : "isabel" },
+    { $rename: { "primerNombre" : "firstName"} }
+)
+//------------ Consultas Avanzadas--------
+
+//--------Ejemplos OR
+db.clientes.find(
+    { $or: [ 
+             { firstName: 'isabel' },
+             { firstName: 'elvia' } 
+           ] 
+    }
+)
+
+//----Menor < que
+db.clientes.find(
+    { age: { $lt: 30 } }
+)
+
+//-----Mayor > que
+db.clientes.find(
+    { age: { $gt: 30 } }
+)
+
+
+
+
+
+
+
+
